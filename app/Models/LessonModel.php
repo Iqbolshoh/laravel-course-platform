6cbf78d53ac083389c54f0c580db01963eab2c78
iqbolshoh <iilhomjonov777@gmail.com>
2025-04-09 10:19:44 +0500

    Create Optimize

---
+++ app/Models/LessonModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LessonModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'lessons';
+
+    protected $fillable = [
+        'course_id',
+        'title',
+        'video_url',
+        'content',
+        'order',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(CourseModel::class, 'course_id');
+    }
+}
