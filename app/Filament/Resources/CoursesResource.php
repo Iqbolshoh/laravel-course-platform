d16eae5130ab06004640d9b2d41f6bdee442458c
iqbolshoh <iilhomjonov777@gmail.com>
2025-04-18 13:27:17 +0500

    commit

---
+++ app/Filament/Resources/CoursesResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CoursesResource\Pages;
+use App\Filament\Resources\CoursesResource\RelationManagers;
+use App\Models\Courses;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CoursesResource extends Resource
+{
+    protected static ?string $model = Courses::class;
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+    protected static ?string $navigationGroup = 'Education';
+    protected static ?int $navigationSort = 6;
+
+    /*
+    |-------------------------------------------------------------------------- 
+    | Access Control Check
+    |-------------------------------------------------------------------------- 
+    */
+    public static function canAccess(string $permission = 'view'): bool
+    {
+        if (!$user = auth()->user())
+            return false;
+
+        return match ($permission) {
+            'view' => $user->can('course.view'),
+            'create' => $user->can('course.create'),
+            'edit' => $user->can('course.edit'),
+            'delete' => $user->can('course.delete'),
+            default => false,
+        };
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCourses::route('/'),
+            'create' => Pages\CreateCourses::route('/create'),
+            'edit' => Pages\EditCourses::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/LessonsResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LessonsResource\Pages;
+use App\Filament\Resources\LessonsResource\RelationManagers;
+use App\Models\Lessons;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LessonsResource extends Resource
+{
+    protected static ?string $model = Lessons::class;
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+    protected static ?string $navigationGroup = 'Education';
+    protected static ?int $navigationSort = 7;
+
+    /*
+    |-------------------------------------------------------------------------- 
+    | Access Control Check
+    |-------------------------------------------------------------------------- 
+    */
+    public static function canAccess(string $permission = 'view'): bool
+    {
+        if (!$user = auth()->user())
+            return false;
+
+        return match ($permission) {
+            'view' => $user->can('lesson.view'),
+            'create' => $user->can('lesson.create'),
+            'edit' => $user->can('lesson.edit'),
+            'delete' => $user->can('lesson.delete'),
+            default => false,
+        };
+    }
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLessons::route('/create'),
+            'edit' => Pages\EditLessons::route('/{record}/edit'),
+        ];
+    }
+}
